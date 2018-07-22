23ffc12ac3f5635399e0382f754fafddee4909ad
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-07-22 20:43:36 +0530

    Adding GUI P1

--- PyDa.py
+++ PyDa.py
@@ -1,5 +1,21 @@
 import wolframalpha
 import wikipedia
+import wx
+
+class MyFrame(wx.Frame):
+    def __init__(self):
+        wx.Frame.__init__(self, None,size
+        pos=wx.DefaultPosition, size=wx.Size(450, 100),
+        style=wx.MINIMIZE_BOX | wx.SYSTEM_MENU | wx.CAPTION |
+        wx.CLOSE_BOX | wx.CLIP_CHILDREN,
+        title="PyDa")
+
+        panel = wx.Panel(self)
+        my_sizer = wx.BoxSizer(wx.VERTICAL)
+        lbl = wx.StaticText(panel,
+        label="This is PyDa, Python virtual Assistant. Write a Question.")
+        my_sizer.Add(lbl, 0, wx.ALL, 5)
+        self.txt = wx.TextCtrl(panel, style = wx.TE_PROCESS_ENTER, size=(400, 30))
 
 while(True):
     inp = input("Question: ")
