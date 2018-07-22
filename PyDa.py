b8ee0de6805b32f3a120ce2f340672ee19736b2f
Prakhar-FF13 <prakhar.meerut@gmail.com>
2018-07-22 20:14:48 +0530

    Importing and Checking the wikipedia API

--- PyDa.py
+++ PyDa.py
@@ -1,4 +1,13 @@
 import wolframalpha
+import wikipedia
 input = input("Question: ")
 app_id = "XJ3PJW-WJHQH7UTQG"
 client = wolframalpha.Client(app_id)
+'''
+res = client.query(input)
+answer = next(res.results).text
+
+print (answer)
+'''
+
+print(wikipedia.summary(input))
